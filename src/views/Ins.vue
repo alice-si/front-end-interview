da09e9b9836f2b5d41a6beba04e168f39df8e796
Joanna Kang <jfrences11@gmail.com>
2021-01-04 19:32:49 +0000

    fix: analysis result conditional rendering logic fixed

--- src/views/Ins.vue
+++ src/views/Ins.vue
@@ -35,19 +35,19 @@
 </template>
 
 <script>
-import LineChart from "../components/LineChart.js";
+import LineChart from '../components/LineChart.js'
 import {
   getCountries,
   getCamps,
   getLessonsByYear
-} from "../data/data-provider.js";
-import "./Ins.css";
+} from '../data/data-provider.js'
+import './Ins.css'
 
 export default {
   components: {
     LineChart
   },
-  data() {
+  data () {
     return {
       chartData: {},
       countries: [],
@@ -56,32 +56,32 @@ export default {
         responsive: true,
         maintainAspectRatio: false
       },
-      current: "",
-      totalLessons: "",
-      arrow: "",
-      growthRate: ""
-    };
+      current: '',
+      totalLessons: '',
+      arrow: '',
+      growthRate: ''
+    }
   },
-  mounted() {
-    this.countries = getCountries();
-    this.changeCountry("Kenya");
-    this.updateLessonSummary();
+  mounted () {
+    this.countries = getCountries()
+    this.changeCountry('Kenya')
+    this.updateLessonSummary()
   },
   methods: {
-    changeCountry: function(value) {
-      this.camps = getCamps(value);
-      this.updateChartData(value);
-      this.current = value;
-      this.updateLessonSummary(value);
+    changeCountry: function (value) {
+      this.camps = getCamps(value)
+      this.updateChartData(value)
+      this.current = value
+      this.updateLessonSummary(value)
     },
-    updateChartData(value) {
-      let lessonsByYear = getLessonsByYear(value);
-      let color = "";
+    updateChartData (value) {
+      let lessonsByYear = getLessonsByYear(value)
+      let color = ''
 
-      if (value === "Kenya") {
-        color = "#EC7181";
-      } else if (value === "Tanzania") {
-        color = "#2cb5f5";
+      if (value === 'Kenya') {
+        color = '#EC7181'
+      } else if (value === 'Tanzania') {
+        color = '#2cb5f5'
       }
 
       this.chartData = {
@@ -89,30 +89,45 @@ export default {
         datasets: [
           {
             label: value,
-            backgroundColor: "transparent",
+            backgroundColor: 'transparent',
             borderColor: color,
             data: lessonsByYear.lessons,
             pointRadius: 6,
-            pointBackgroundColor: "#FFFFFF",
+            pointBackgroundColor: '#FFFFFF',
             lineTension: 0
           }
         ]
-      };
+      }
     },
-    updateLessonSummary(value) {
-      let lessonsByYear = getLessonsByYear(value);
+    updateLessonSummary (value) {
+      let lessonsByYear = getLessonsByYear(value)
       if (lessonsByYear.lessons.length > 0) {
         this.totalLessons = lessonsByYear.lessons.reduce(
           (prev, curr) => prev + curr
-        );
+        )
 
-        const recentYears = lessonsByYear.lessons.slice(-2);
-        const growthRate = (recentYears[1] / recentYears[0] - 1) * 100;
+        const recentYears = lessonsByYear.lessons.slice(-2)
+        const oneYearBefore = recentYears[0]
+        const twoYearBefore = recentYears[1]
+        const growthRate = (twoYearBefore / oneYearBefore - 1) * 100
 
-        growthRate > 0 ? (this.arrow = "⬆") : (this.arrow = "⬇");
-        this.growthRate = growthRate.toFixed(0);
+        if (oneYearBefore === 0) {
+          this.arrow = '⬇'
+          this.growthRate = '100'
+        }
+
+        if (growthRate !== Infinity) {
+          if (growthRate > 0) {
+            this.arrow = '⬆'
+          } else if (growthRate < 0) {
+            this.arrow = '⬇'
+          } else if (growthRate === 0) {
+            this.arrow = ''
+          }
+          this.growthRate = growthRate.toFixed(0)
+        }
       }
     }
   }
-};
+}
 </script>
